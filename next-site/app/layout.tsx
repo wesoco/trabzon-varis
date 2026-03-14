5d7389b2802ca36c96a45384daf5fc2bbc4749ec
ilhan <ilhan.eyubler@wesoco.com>
2026-03-15 02:31:47 +0300

    add gtm

--- next-site/app/layout.tsx
+++ next-site/app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Outfit } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { buildOrganizationSchema, getBaseUrl } from "@/lib/schema";
 import { SITE_CONFIG } from "@/lib/content";
+
+const GTM_ID = "GTM-NLVXNRCC";
 
 const outfit = Outfit({
   variable: "--font-outfit",
@@ -89,6 +92,26 @@ export default function RootLayout({
         />
       </head>
       <body className={`${outfit.variable} antialiased`}>
+        <Script
+          id="gtm"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`,
+          }}
+        />
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+            title="Google Tag Manager"
+          />
+        </noscript>
         <div className="page-container">
           <Header />
           {children}
